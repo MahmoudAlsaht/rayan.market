8376d42a2c6cbf823d4c1161e2d6a257d55f08d1
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2023-09-27 15:41:34 +0300

    fix some bugs in mainNavbar and add logout function to the mainNavbar

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -3,13 +3,25 @@ import {
 	Nav,
 	Navbar,
 	NavDropdown,
-	Form,
-	Button,
 } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { BsFillPersonFill } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+
+import { useAppDispatch } from '../app/hooks';
+
+import { fetchUser, logOut } from '../controllers/user';
 
 function MainNavbar() {
+	const dispatch = useAppDispatch();
+
+	const navigate = useNavigate();
+
+	const handleLogout = async () => {
+		await logOut();
+		dispatch(fetchUser());
+		navigate('/');
+	};
+
 	return (
 		<Navbar
 			collapseOnSelect
@@ -17,50 +29,38 @@ function MainNavbar() {
 			className='bg-white shadow mb-3'
 		>
 			<Container>
-				<Navbar.Brand>
-					<Link to='/'>React-Bootstrap</Link>
+				<Navbar.Brand href='/'>
+					React-Bootstrap
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 				<Navbar.Collapse id='responsive-navbar-nav'>
 					<Nav className='me-auto'>
-						<Nav.Link href='#features'>
-							<Link to='/'>Home</Link>
-						</Nav.Link>
-						<Nav.Link href='#pricing'>
-							<Link to='/about'>About</Link>
-						</Nav.Link>
+						<Nav.Link href='/'>Home</Nav.Link>
+						<Nav.Link href='/about'>About</Nav.Link>
+					</Nav>
+					<Nav className='ms-auto'>
+						<NavDropdown
+							title={<BsFillPersonFill />}
+							id='collapsible-nav-dropdown'
+						>
+							<NavDropdown.Item href='/auth/signup'>
+								Signup
+							</NavDropdown.Item>
+							<NavDropdown.Item href='/auth/signin'>
+								Signin
+							</NavDropdown.Item>
+							<NavDropdown.Item
+								onClick={handleLogout}
+							>
+								Logout
+							</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href='/profile'>
+								Profile
+							</NavDropdown.Item>
+						</NavDropdown>
 					</Nav>
 				</Navbar.Collapse>
-				<Form className='d-flex me-5'>
-					<Form.Control
-						type='search'
-						placeholder='Search'
-						className='me-2'
-						aria-label='Search'
-					/>
-					<Button variant='outline-success'>
-						Search
-					</Button>
-				</Form>
-
-				<NavDropdown
-					title={<BsFillPersonFill />}
-					id='collapsible-nav-dropdown'
-				>
-					<NavDropdown.Item>
-						<Link to='/auth/signup'>Signup</Link>
-					</NavDropdown.Item>
-					<NavDropdown.Item>
-						<Link to='/auth/signin'>Signin</Link>
-					</NavDropdown.Item>
-					<NavDropdown.Item>
-						<Link to='#'>Logout</Link>
-					</NavDropdown.Item>
-					<NavDropdown.Divider />
-					<NavDropdown.Item>
-						<Link to='/profile'>Profile</Link>
-					</NavDropdown.Item>
-				</NavDropdown>
 			</Container>
 		</Navbar>
 	);
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,31 +1,22 @@
+import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { IUser, fetchUser, logOut } from '../controllers/user';
-import { useNavigate } from 'react-router-dom';
+import { IUser, fetchUser } from '../controllers/user';
 
 function Home() {
 	const dispatch = useAppDispatch();
 	const user: IUser | any = useAppSelector(
 		(state) => state.user,
 	);
-	const navigate = useNavigate();
 
-	const handleLogout = async () => {
-		await logOut();
+	useEffect(() => {
 		dispatch(fetchUser());
-		navigate('/');
-	};
+	}, []);
 
 	return (
 		<>
 			<h1 className='text-danger'>
 				{user !== null ? user.displayName : ''}
 			</h1>
-			<button onClick={() => dispatch(fetchUser())}>
-				Get User
-			</button>
-			<div>
-				<button onClick={handleLogout}>Logout</button>
-			</div>
 		</>
 	);
 }
