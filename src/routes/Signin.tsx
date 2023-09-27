eaf864e58be5105c7ba452472d4e9b10338ddce5
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2023-09-27 15:42:03 +0300

    edit signup and signin form style

--- src/routes/Signin.tsx
+++ src/routes/Signin.tsx
@@ -1,6 +1,8 @@
 import { useRef, FormEvent } from 'react';
 import { signIn } from '../controllers/user';
 import { useNavigate } from 'react-router-dom';
+import { Container, Form, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function SignIn() {
 	const emailRef = useRef<any>(null);
@@ -23,20 +25,62 @@ function SignIn() {
 
 	return (
 		<>
-			<form
-				onSubmit={handleSubmit}
-				style={{ margin: '10rem 10rem' }}
-			>
-				<div>
-					<input type='email' ref={emailRef} />
-				</div>
-				<div>
-					<input type='password' ref={passwordRef} />
-				</div>
-				<div>
-					<input type='submit' value='Sign In' />
-				</div>
-			</form>
+			<Row className='d-flex justify-content-center'>
+				<Col xs={10} sm={8} md={6} lg={5} xl={4}>
+					<Container className='bg-white mt-5 p-5 border rounded shadow'>
+						<Form
+							onSubmit={handleSubmit}
+							style={{ width: '100%' }}
+						>
+							<Form.Group
+								className='mb-3'
+								controlId='emailInput'
+							>
+								<Form.Label>
+									Email address
+								</Form.Label>
+								<Form.Control
+									type='email'
+									placeholder='name@example.com'
+									ref={emailRef}
+								/>
+							</Form.Group>
+
+							<Form.Group
+								className='mb-3'
+								controlId='passwordInput'
+							>
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type='password'
+									placeholder='at least 6 char'
+									ref={passwordRef}
+								/>
+							</Form.Group>
+							<Form.Group className='mb-3'>
+								<small className='text-info m-2'>
+									Don't have an account?{' '}
+									<Link
+										to='/auth/signup'
+										className='text-primary'
+										style={{
+											textDecoration:
+												'underline',
+										}}
+									>
+										SIGNUP
+									</Link>
+								</small>
+								<Form.Control
+									type='submit'
+									value='Signin'
+									className='btn btn-outline-primary'
+								/>
+							</Form.Group>
+						</Form>
+					</Container>
+				</Col>
+			</Row>
 		</>
 	);
 }
--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,21 +1,27 @@
 import { useRef, FormEvent } from 'react';
 import { signUp } from '../controllers/user';
 import { useNavigate } from 'react-router-dom';
+import { Container, Form, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Signup() {
-	const emailRef = useRef<any>(null);
-	const displayNameRef = useRef<any>(null);
-	const passwordRef = useRef<any>(null);
+	const emailRef = useRef<HTMLInputElement>(null);
+	const displayNameRef = useRef<HTMLInputElement>(null);
+	const passwordRef = useRef<HTMLInputElement>(null);
 	const navigate = useNavigate();
 
 	const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault();
 
+		console.log(emailRef.current?.value);
+		console.log(passwordRef.current?.value);
+		console.log(displayNameRef.current?.value);
+
 		try {
 			await signUp(
-				emailRef.current.value,
-				passwordRef.current.value,
-				displayNameRef.current.value,
+				emailRef.current?.value,
+				passwordRef.current?.value,
+				displayNameRef.current!.value,
 			);
 			navigate('/');
 		} catch (e: any) {
@@ -24,25 +30,73 @@ function Signup() {
 	};
 
 	return (
-		<>
-			<form
-				onSubmit={handleSubmit}
-				style={{ margin: '10rem 10rem' }}
-			>
-				<div>
-					<input type='email' ref={emailRef} />
-				</div>
-				<div>
-					<input type='text' ref={displayNameRef} />
-				</div>
-				<div>
-					<input type='password' ref={passwordRef} />
-				</div>
-				<div>
-					<input type='submit' value='Register' />
-				</div>
-			</form>
-		</>
+		<Row className='d-flex justify-content-center'>
+			<Col xs={10} sm={8} md={6} lg={5} xl={4}>
+				<Container className='bg-white mt-5 p-5 border rounded shadow'>
+					<Form
+						onSubmit={handleSubmit}
+						style={{ width: '100%' }}
+					>
+						<Form.Group
+							className='mb-3'
+							controlId='emailInput'
+						>
+							<Form.Label>
+								Email address
+							</Form.Label>
+							<Form.Control
+								type='email'
+								placeholder='name@example.com'
+								ref={emailRef}
+							/>
+						</Form.Group>
+
+						<Form.Group
+							className='mb-3'
+							controlId='NameInput'
+						>
+							<Form.Label>Name</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='your name'
+								ref={displayNameRef}
+							/>
+						</Form.Group>
+						<Form.Group
+							className='mb-3'
+							controlId='passwordInput'
+						>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='at least 6 char'
+								ref={passwordRef}
+							/>
+						</Form.Group>
+						<Form.Group className='mb-3'>
+							<small className='text-info m-3'>
+								Already have an account?{' '}
+								<Link
+									to='/auth/signin'
+									className='text-primary'
+									style={{
+										textDecoration:
+											'underline',
+									}}
+								>
+									SIGNIN
+								</Link>
+							</small>
+							<Form.Control
+								type='submit'
+								value='Signup'
+								className='btn btn-outline-primary'
+							/>
+						</Form.Group>
+					</Form>
+				</Container>
+			</Col>
+		</Row>
 	);
 }
 
