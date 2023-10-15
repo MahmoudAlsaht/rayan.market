20064061d77860172f9c7c0a452662cc7fc4969f
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2023-10-15 15:51:52 +0300

    move user forms into a separate folder

---
+++ src/components/forms/DeleteUser.tsx
@@ -0,0 +1,143 @@
+import { useState, useRef, FormEvent, ChangeEvent } from 'react';
+import ErrorComponent, { IError } from '../Error';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { destroyUser } from '../../controllers/profile';
+import { useNavigate } from 'react-router-dom';
+import LoadingButton from '../LoadingButton';
+
+function DeleteUser({
+	profileId,
+	isLoading,
+	setIsLoading,
+}: {
+	profileId: string;
+	isLoading: boolean;
+	setIsLoading: (status: boolean) => void;
+}) {
+	const [validated, setValidated] = useState(false);
+	const [error, setError] = useState<IError>({
+		status: null,
+		message: '',
+	});
+	const passwordRef = useRef<HTMLInputElement>(null);
+
+	const navigate = useNavigate();
+
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const handleRemoveUser = async (e: FormEvent) => {
+		e.preventDefault();
+		try {
+			setIsLoading(true);
+			const form = e.currentTarget as HTMLFormElement;
+			if (form.checkValidity() === false) {
+				setError({
+					status: true,
+					message: 'invalid fields',
+				});
+			} else {
+				await destroyUser(
+					passwordRef.current?.value as string,
+					profileId,
+				);
+				handleClose();
+				setIsLoading(false);
+				navigate('/');
+			}
+		} catch (e: any) {
+			setError({
+				status: true,
+				message: e.message,
+			});
+			setIsLoading(false);
+		}
+	};
+
+	const handleChange = (e: ChangeEvent) => {
+		const form = e.currentTarget as HTMLFormElement;
+		if (
+			passwordRef.current?.value === '' ||
+			form.checkValidity() === false
+		) {
+			setValidated(false);
+			setError({
+				status: false,
+				message: 'please provide all the missing fields',
+			});
+		} else {
+			setValidated(true);
+			setError({
+				status: true,
+				message: 'looks good!',
+			});
+		}
+	};
+
+	return (
+		<>
+			<h3 className='text-muted mb-3'>Account Deletion</h3>
+
+			<ErrorComponent error={error} />
+
+			<Button
+				variant={
+					!isLoading ? 'outline-danger' : 'secondary'
+				}
+				onClick={handleShow}
+				disabled={isLoading}
+			>
+				Delete User
+			</Button>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title className='text-danger'>
+						Delete Account?
+					</Modal.Title>
+				</Modal.Header>
+				<Form
+					noValidate
+					validated={!validated}
+					onSubmit={handleRemoveUser}
+					style={{ width: '100%' }}
+				>
+					<Modal.Body className='text-danger'>
+						Type Your Password To Confirm Deletion
+						<Form.Group
+							className='mt-3 mb-3'
+							controlId='passwordUserInfoFormInput'
+						>
+							<Form.Control
+								required
+								onChange={handleChange}
+								type='password'
+								placeholder='at least 6 char'
+								ref={passwordRef}
+							/>
+						</Form.Group>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button
+							variant='secondary'
+							onClick={handleClose}
+						>
+							Close
+						</Button>
+						<LoadingButton
+							type='submit'
+							body='Delete Account'
+							variant='danger'
+							className={'w-50'}
+							isLoading={isLoading}
+						/>
+					</Modal.Footer>
+				</Form>
+			</Modal>
+		</>
+	);
+}
+
+export default DeleteUser;
---
+++ src/components/forms/UpdateUserInfoForm.tsx
@@ -0,0 +1,143 @@
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { IUser } from '../../controllers/user';
+import { updateUserInfo } from '../../controllers/profile';
+import ErrorComponent, { IError } from '../Error';
+import LoadingButton from '../LoadingButton';
+
+function UpdateUserInfoForm({
+	profileOwner,
+	isLoading,
+	setIsLoading,
+}: {
+	profileOwner: IUser;
+	isLoading: boolean;
+	setIsLoading: (status: boolean) => void;
+}) {
+	const [validated, setValidated] = useState(false);
+	const [error, setError] = useState<IError>({
+		status: null,
+		message: '',
+	});
+
+	const emailRef = useRef<HTMLInputElement>(null);
+	const usernameRef = useRef<HTMLInputElement>(null);
+	const passwordRef = useRef<HTMLInputElement>(null);
+
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+
+		try {
+			setIsLoading(true);
+			const form = e.currentTarget as HTMLFormElement;
+
+			if (form.checkValidity() === false) {
+				setError({
+					status: true,
+					message: 'invalid fields',
+				});
+			} else {
+				const data = {
+					currentPassword: passwordRef.current?.value,
+					email: emailRef.current?.value,
+					username: usernameRef.current?.value,
+				};
+				await updateUserInfo(data, profileOwner?.docId);
+				setIsLoading(false);
+				emailRef.current!.value = '';
+				usernameRef.current!.value = '';
+				passwordRef.current!.value = '';
+			}
+		} catch (e: any) {
+			setError({
+				status: true,
+				message: e.message,
+			});
+			setIsLoading(false);
+		}
+	};
+
+	const handleChange = (e: ChangeEvent) => {
+		const form = e.currentTarget as HTMLFormElement;
+		if (
+			passwordRef.current?.value === '' ||
+			emailRef.current?.value === '' ||
+			usernameRef.current?.value === '' ||
+			form.checkValidity() === false
+		) {
+			setValidated(false);
+			setError({
+				status: false,
+				message: 'please provide all the missing fields',
+			});
+		} else {
+			setValidated(true);
+			setError({
+				status: true,
+				message: 'looks good!',
+			});
+		}
+	};
+
+	return (
+		<Form
+			noValidate
+			validated={!validated}
+			onSubmit={handleSubmit}
+			style={{ width: '100%' }}
+		>
+			<h3 className='text-muted mb-3'>Personal Info</h3>
+
+			<ErrorComponent error={error} />
+
+			<Form.Group className='mb-3' controlId='emailInput'>
+				<Form.Label>Email address</Form.Label>
+				<Form.Control
+					onChange={handleChange}
+					type='email'
+					required
+					placeholder='name@example.com'
+					defaultValue={profileOwner?.email as string}
+					ref={emailRef}
+				/>
+			</Form.Group>
+
+			<Form.Group className='mb-3' controlId='NameInput'>
+				<Form.Label>username</Form.Label>
+				<Form.Control
+					required
+					onChange={handleChange}
+					type='text'
+					placeholder='your name'
+					defaultValue={
+						profileOwner?.username as string
+					}
+					ref={usernameRef}
+				/>
+			</Form.Group>
+			<Form.Group
+				className='mb-3'
+				controlId='passwordUserInfoFormInput'
+			>
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					required
+					onChange={handleChange}
+					type='password'
+					placeholder='at least 6 char'
+					ref={passwordRef}
+				/>
+			</Form.Group>
+			<Form.Group className='mb-3'>
+				<LoadingButton
+					type='submit'
+					body='Update'
+					variant='primary'
+					isLoading={isLoading}
+				/>
+			</Form.Group>
+		</Form>
+	);
+}
+
+export default UpdateUserInfoForm;
