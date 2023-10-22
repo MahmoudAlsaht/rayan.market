2d8eda5ac4a8c22c84d6ae70a5a2b63d4269b068
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2023-10-22 15:42:24 +0300

    show all categories

--- src/routes/store/Categories.tsx
+++ src/routes/store/Categories.tsx
@@ -1,8 +1,33 @@
 import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { ICategory } from '../../app/store/category';
+import { fetchCategories } from '../../controllers/category';
 
 function Categories() {
-	useEffect(() => {}, []);
-	return <div>Categories</div>;
+	const dispatch = useAppDispatch();
+	const categories: ICategory[] | null = useAppSelector(
+		(state) => state.categories,
+	);
+
+	useEffect(() => {
+		dispatch(fetchCategories());
+	}, [dispatch]);
+
+	return (
+		<>
+			<ul>
+				{categories != null &&
+					categories.map((category: ICategory) => (
+						<li
+							key={category.id}
+							className='text-danger'
+						>
+							{category.name}
+						</li>
+					))}
+			</ul>
+		</>
+	);
 }
 
 export default Categories;
