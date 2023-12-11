44e74a6bc19d27849ca4f3f2963fd0da48a3ee54
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2023-12-11 14:50:16 +0300

    alter widget component to have a ReactNode child

--- src/components/dashboardComponents/Widget.tsx
+++ src/components/dashboardComponents/Widget.tsx
@@ -1,31 +1,52 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { ReactNode } from 'react';
 import { Card } from 'react-bootstrap';
 import { BsArrowRight } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 
 type widgetProps = {
-	widgetTitle: string;
+	widgetTitle: string | ReactNode;
 	className?: string;
 	href: string;
 };
 
 function Widget({ widgetTitle, className, href }: widgetProps) {
+	const isTitleAString = typeof widgetTitle === 'string';
+
 	return (
 		<>
-			<Card className={`widget ${className}`}>
-				<Card.Body>
-					<Card.Title>{widgetTitle}</Card.Title>
-					<Card.Text>
-						<Link to={href}>
-							<legend
-								className='card-link lead'
-								style={{ color: '#0055aa' }}
+			<Link to={href}>
+				<Card
+					className={`widget ${className}`}
+					style={{ height: '100px' }}
+				>
+					<Card.Body>
+						{isTitleAString ? (
+							<Card.Title>
+								{widgetTitle}
+							</Card.Title>
+						) : (
+							<Card.Title
+								className='text-center'
+								style={{ fontSize: '50px' }}
 							>
-								see more <BsArrowRight />
-							</legend>
-						</Link>
-					</Card.Text>
-				</Card.Body>
-			</Card>
+								{widgetTitle}
+							</Card.Title>
+						)}
+
+						<Card.Text>
+							{isTitleAString && (
+								<legend
+									className='card-link lead'
+									style={{ color: '#0055aa' }}
+								>
+									see more <BsArrowRight />
+								</legend>
+							)}
+						</Card.Text>
+					</Card.Body>
+				</Card>
+			</Link>
 		</>
 	);
 }
