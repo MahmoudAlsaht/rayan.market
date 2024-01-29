088006aad44fca451a3839430acd8b6d0799d54c
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-01-29 14:13:24 +0300

    create a productCartActions component and move the functionality of (crud) product in cart to it

--- src/components/CartProductCard.tsx
+++ src/components/CartProductCard.tsx
@@ -1,16 +1,8 @@
-import { Link } from 'react-router-dom';
-import { useAppDispatch } from '../app/hooks';
-import {
-	TCartProduct,
-	addToCounter,
-	removeFromCounter,
-	removeProduct,
-	updateTotalPrice,
-} from '../app/store/cart';
-import { Badge, Image, Pagination } from 'react-bootstrap';
-import { BsTrash } from 'react-icons/bs';
+import { TCartProduct } from '../app/store/cart';
+import { Badge, Image } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import { sumEachProductTotalPrice } from '../utils';
+import ProductCartActions from './ProductCartActions';
 
 type CartProductCardProps = {
 	product: TCartProduct | null;
@@ -21,39 +13,12 @@ function CartProductCard({
 	product,
 	type,
 }: CartProductCardProps) {
-	const dispatch = useAppDispatch();
 	const [totalProductPrice, setTotalProductPrice] =
 		useState(0);
 
 	useEffect(() => {
 		setTotalProductPrice(sumEachProductTotalPrice(product!));
 	}, [product]);
-
-	const handleAddProduct = () => {
-		dispatch(
-			addToCounter({
-				id: product?.id as string,
-				maxNum: parseInt(product?.price as string),
-			}),
-		);
-		dispatch(
-			updateTotalPrice(parseInt(product?.price as string)),
-		);
-	};
-
-	const handleRemoveProduct = () => {
-		dispatch(removeFromCounter(product?.id as string));
-		dispatch(
-			updateTotalPrice(
-				-parseInt(product?.price as string),
-			),
-		);
-	};
-
-	const handleDestroyProduct = () => {
-		dispatch(removeProduct(product?.id as string));
-		dispatch(updateTotalPrice(-totalProductPrice));
-	};
 
 	return (
 		<>
@@ -81,28 +46,10 @@ function CartProductCard({
 						</h6>
 					)}
 					{type !== 'cartSummary' && (
-						<Pagination>
-							<Pagination.Item
-								onClick={handleRemoveProduct}
-							>
-								-
-							</Pagination.Item>
-							<Pagination.Item>
-								{product?.counter}
-							</Pagination.Item>
-							<Pagination.Item
-								onClick={handleAddProduct}
-							>
-								+
-							</Pagination.Item>
-							<Link
-								to='#'
-								className='border p-1'
-								onClick={handleDestroyProduct}
-							>
-								<BsTrash className='text-danger' />
-							</Link>
-						</Pagination>
+						<ProductCartActions
+							totalProductPrice={totalProductPrice}
+							product={product}
+						/>
 					)}
 					<span className='text-muted'>
 						Total Price:{' '}
---
+++ src/components/ProductCartActions.tsx
@@ -0,0 +1,72 @@
+import { Pagination } from 'react-bootstrap';
+import { BsTrash } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import {
+	TCartProduct,
+	addToCounter,
+	removeFromCounter,
+	removeProduct,
+	updateTotalPrice,
+} from '../app/store/cart';
+import { useAppDispatch } from '../app/hooks';
+
+type ProductCartActionsProps = {
+	product: TCartProduct | null;
+	totalProductPrice: number;
+};
+
+function ProductCartActions({
+	product,
+	totalProductPrice,
+}: ProductCartActionsProps) {
+	const dispatch = useAppDispatch();
+
+	const handleAddProduct = () => {
+		if (product?.counter == product?.quantity) return;
+		dispatch(
+			addToCounter({
+				id: product?.id as string,
+				maxNum: parseInt(product?.price as string),
+			}),
+		);
+		dispatch(
+			updateTotalPrice(parseInt(product?.price as string)),
+		);
+	};
+
+	const handleRemoveProduct = () => {
+		if (product?.counter === 0) return;
+		dispatch(removeFromCounter(product?.id as string));
+		dispatch(
+			updateTotalPrice(
+				-parseInt(product?.price as string),
+			),
+		);
+	};
+
+	const handleDestroyProduct = () => {
+		dispatch(removeProduct(product?.id as string));
+		dispatch(updateTotalPrice(-totalProductPrice));
+	};
+
+	return (
+		<Pagination>
+			<Pagination.Item onClick={handleRemoveProduct}>
+				-
+			</Pagination.Item>
+			<Pagination.Item>{product?.counter}</Pagination.Item>
+			<Pagination.Item onClick={handleAddProduct}>
+				+
+			</Pagination.Item>
+			<Link
+				to='#'
+				className='border p-1'
+				onClick={handleDestroyProduct}
+			>
+				<BsTrash className='text-danger' />
+			</Link>
+		</Pagination>
+	);
+}
+
+export default ProductCartActions;
