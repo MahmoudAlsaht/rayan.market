ac67a2d099ad6a804ef566bffd6bbba66b74bf06
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-02-18 10:15:06 +0300

    fix a build issue in ContactInformation.tsx

--- src/components/checkoutComponents/ContactInformation.tsx
+++ src/components/checkoutComponents/ContactInformation.tsx
@@ -1,4 +1,14 @@
-
+import { Breadcrumb, Col } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { TProfile } from '../../app/auth/profile';
+import {
+	TContactInfo,
+	getContactsData,
+} from '../../controllers/contact';
+import AnonymousUserForm from '../forms/AnonymousUserForm';
+import ChooseContactAddress from './ChooseContactAddress';
+import { Link } from 'react-router-dom';
+import Logo from '../../rayan.marketLogo.png';
 
 function ContactInformation({
 	handleStep,
@@ -7,7 +17,8 @@ function ContactInformation({
 	handleStep: (step: string) => void;
 	profile: TProfile | null;
 }) {
-	
+	const [contacts, setContacts] =
+		useState<(TContactInfo | null)[]>();
 
 	useEffect(() => {
 		if (profile) {
@@ -22,7 +33,52 @@ function ContactInformation({
 	}, [profile]);
 
 	return (
-		
+		<Col xs={12} md={6} className='checkoutInformation'>
+			<h1 className='logo mb-5'>
+				<Link to='/home' className='text-info'>
+					<img
+						src={Logo}
+						alt='Al Rayyan International Markets'
+						width={90}
+					/>
+				</Link>
+			</h1>
+			<Breadcrumb className='mb-5'>
+				<Breadcrumb.Item>
+					<Link to='/cart' className='text-dark'>
+						Cart
+					</Link>
+				</Breadcrumb.Item>
+				<Breadcrumb.Item
+					className='text-info'
+					active
+					onClick={() => handleStep('information')}
+				>
+					Contact &amp; shipping
+				</Breadcrumb.Item>
+			</Breadcrumb>
+			<hr />
+
+			{profile?._id === '' ? (
+				<div>
+					<small>
+						Do You want to sign in?{' '}
+						<Link to='/auth/signin'>Signin</Link>
+					</small>
+					<h3 className='m-3'>Contact</h3>
+					<AnonymousUserForm
+						contact={undefined}
+						handleStep={handleStep}
+					/>
+				</div>
+			) : (
+				<ChooseContactAddress
+					contacts={contacts!}
+					profile={profile}
+					handleStep={handleStep}
+				/>
+			)}
+		</Col>
 	);
 }
 
