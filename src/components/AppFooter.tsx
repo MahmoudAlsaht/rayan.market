cbf2e004ceb627f7fe99bfd9f51395116c0010e9
Mahmoud Alsaht <mahmoudr2097@gmail.com>
2024-02-27 13:41:55 +0300

    change AppFooter backgroundColor

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,11 +1,6 @@
-import {
-	createTheme,
-	ThemeProvider,
-} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import {
 	Facebook,
 	Twitter,
@@ -17,56 +12,45 @@ function Copyright() {
 	return (
 		<Typography variant='body2' color='text.secondary'>
 			{'Copyright © '}
-			<Link color='inherit' href='#'>
-				Alrayan Markets
-			</Link>{' '}
-			{new Date().getFullYear()}
+			Alrayan Markets {new Date().getFullYear()}
 			{'.'}
 		</Typography>
 	);
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
-
 export default function StickyFooter() {
 	return (
-		<ThemeProvider theme={defaultTheme}>
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				minHeight: '100vh',
+			}}
+		>
 			<Box
+				component='footer'
 				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					minHeight: '100vh',
+					py: 3,
+					px: 2,
+					mt: 'auto',
+					backgroundColor: '#07a18033',
 				}}
 			>
-				<Box
-					component='footer'
-					sx={{
-						py: 3,
-						px: 2,
-						mt: 'auto',
-						backgroundColor: (theme) =>
-							theme.palette.mode === 'light'
-								? theme.palette.grey[200]
-								: theme.palette.grey[800],
-					}}
-				>
-					<Container maxWidth='sm'>
-						<Typography variant='body1'>
-							<IconButton>
-								<Facebook />
-							</IconButton>
-							<IconButton>
-								<Twitter />
-							</IconButton>
-							<IconButton>
-								<Instagram />
-							</IconButton>
-						</Typography>
-						<Copyright />
-					</Container>
-				</Box>
+				<Container maxWidth='sm'>
+					<Typography variant='body1'>
+						<IconButton>
+							<Facebook />
+						</IconButton>
+						<IconButton>
+							<Twitter />
+						</IconButton>
+						<IconButton>
+							<Instagram />
+						</IconButton>
+					</Typography>
+					<Copyright />
+				</Container>
 			</Box>
-		</ThemeProvider>
+		</Box>
 	);
 }
